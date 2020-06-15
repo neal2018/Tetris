ccabba5ce3ee459b9715d83d6915098164303819
neal2018 <neal.lion@icloud.com>
2020-06-15 18:02:50 +0800

    move on

--- TetrisUnity/Assets/Codes/UI/dmBlockBuilder.cs
+++ TetrisUnity/Assets/Codes/UI/dmBlockBuilder.cs
@@ -1,34 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
- 
-public class dmBlockBuilder : MonoBehaviour {
-    public List<dmBlockBase> blockBaseList;
-    public GameObject squarePrefab;
-    public Vector2 squareSize;
-    [HideInInspector]
-    public dmBlock nowBlock;
-    public void BuildRandomBlock()
-    {
-        if(nowBlock!=null)DestroySquares(nowBlock);
-        dmBlockBase inBuildingBlock = blockBaseList[Random.Range(0, blockBaseList.Count)];
-        nowBlock = new dmBlock();
-        nowBlock.InitBlock(inBuildingBlock);
-        foreach (Vector2 vec in inBuildingBlock.squareCoordList)
-        {
-            GameObject newSquare = Instantiate(squarePrefab);
-            newSquare.transform.SetParent(transform);
-            newSquare.transform.localPosition = new Vector3(squareSize.x * vec.x, -squareSize.x * vec.y);
-            newSquare.GetComponent<RectTransform>().sizeDelta = squareSize;
-            nowBlock.squareList.Add(newSquare);
-        }
-    }
-    public void DestroySquares(dmBlock block)
-    {
-        foreach(GameObject squareInstance in block.squareList)
-        {
-            Destroy(squareInstance);
-        }
-    }
+
+
+public class dmBlockBuilder : MonoBehaviour
+{
+    public List<dmBlockBase> blockBaseList;
+    public GameObject squarePrefab;
+    public Vector2 squareSize;
+    public Vector2 genePos;
+    [HideInInspector]
+    public dmBlock nowBlock;
+
+    void Start()
+    {
+    }
+    public void BuildRandomBlock()
+    {
+        if (nowBlock != null)
+        {
+            DestroySquares(nowBlock);
+        }
+
+        dmBlockBase inBuildingBlock = blockBaseList[Random.Range(0, blockBaseList.Count)];
+        nowBlock = new dmBlock();
+        nowBlock.InitBlock(inBuildingBlock);
+        foreach (Vector2 vec in nowBlock.bindBase.squareCoordList)
+        {
+            GameObject newSquare = Instantiate(squarePrefab);
+            newSquare.transform.SetParent(transform);
+            newSquare.GetComponent<RectTransform>().sizeDelta = squareSize;
+            newSquare.transform.localPosition = genePos +  new Vector2(squareSize.x * vec.x * 2.5f, -squareSize.x * vec.y * 2.5f);
+            Debug.Log(vec);
+            nowBlock.squareList.Add(newSquare);
+        }
+    }
+
+    public void DestroySquares(dmBlock block)
+    {
+        foreach (GameObject squareInstance in block.squareList)
+        {
+            Destroy(squareInstance);
+        }
+    }
 }
--- TetrisUnity/Assets/Codes/UI/dmGameArea.cs
+++ TetrisUnity/Assets/Codes/UI/dmGameArea.cs
@@ -1,26 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class dmGameArea : MonoBehaviour
 {
     public dmBlockBuilder blockBuilder;
     public Vector2 areaSize;
     public Vector2 initPos;
+    [Range(1, 10)]
+    public float fallSpeed;
     RectTransform myRectTransform { get { return GetComponent<RectTransform>(); } }
     dmBlock nowControlBlock;
+    bool runFlag;
+    float fallInteval { get { return 1 / fallSpeed; } }
+    float timeCount;
+    Vector2 nowBlockPos;
+    Dictionary<Vector2, GameObject> squareMap = new Dictionary<Vector2, GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
         myRectTransform.sizeDelta = new Vector2(areaSize.x * blockBuilder.squareSize.x,
                                                 areaSize.y * blockBuilder.squareSize.y);
+        runFlag = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (runFlag)
+        {
+            timeCount += Time.deltaTime;
+            if (timeCount >= fallInteval)
+            {
+                timeCount -= fallInteval;
+                DoFall();
+            }
+        }
+    }
 
+    public void DoFall()
+    {
+        Vector2 targetPos = nowBlockPos + new Vector2(0, 1);
+        if (!CheckCollision(nowControlBlock, targetPos))
+        {
+            MoveBlockTo(nowControlBlock, targetPos);
+        }
+        else
+        {
+            FallGround(nowControlBlock);
+            NextBlock();
+        }
+    }
+
+    public bool CheckCollision(dmBlock block, Vector2 targetPos)
+    {
+        foreach (Vector2 squareCoord in block.bindBase.squareCoordList)
+        {
+            Vector2 squarePos = squareCoord + targetPos;
+            if (squarePos.y >= areaSize.y || squarePos.x < 0 || squarePos.x >= areaSize.x)
+            {
+                return true;
+            }
+
+            if (squareMap.ContainsKey(squarePos))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void FallGround(dmBlock block)
+    {
+        for (int i = 0; i < block.bindBase.squareCoordList.Count; i++)
+        {
+            squareMap.Add(nowBlockPos + block.bindBase.squareCoordList[i], block.squareList[i]);
+            block.squareList[i].GetComponentInChildren<Image>().color = Color.gray;
+        }
     }
 
     public void StartGame()
@@ -29,8 +87,18 @@ public class dmGameArea : MonoBehaviour
         {
             blockBuilder.DestroySquares(nowControlBlock);
         }
+        if (squareMap != null)
+        {
+            foreach (Vector2 squareCoord in squareMap.Keys)
+            {
+                Destroy(squareMap[squareCoord]);
+            }
+        }
+        squareMap.Clear();
         blockBuilder.BuildRandomBlock();
         NextBlock();
+        timeCount = 0;
+        runFlag = true;
     }
 
     public void NextBlock()
@@ -52,17 +120,25 @@ public class dmGameArea : MonoBehaviour
 
     public void MoveBlockTo(dmBlock onMoveBlock, Vector2 targetPos)
     {
+        if (CheckCollision(onMoveBlock, targetPos))
+        {
+            runFlag = false;
+            return;
+        }
+
         for (int i = 0; i < onMoveBlock.bindBase.squareCoordList.Count; i++)
         {
             Vector2 squareCoord = targetPos + onMoveBlock.bindBase.squareCoordList[i];
             onMoveBlock.squareList[i].transform.localPosition = AreaPos2Local(squareCoord);
             onMoveBlock.squareList[i].SetActive(squareCoord.y >= 0);
         }
+
+        nowBlockPos = targetPos;
     }
 
     public Vector2 AreaPos2Local(Vector2 areaPos)
     {
-        return new Vector2((areaPos.x + 0.5f) * blockBuilder.squareSize.x,
-                        -(areaPos.y + 0.5f) * blockBuilder.squareSize.y);
+        return new Vector2((areaPos.x) * blockBuilder.squareSize.x * 2.5f,
+                        -(areaPos.y) * blockBuilder.squareSize.y * 2.5f);
     }
 }
